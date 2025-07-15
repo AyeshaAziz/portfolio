c568d484f23b4cae506c342e34f3ee8290a98a8b
AyeshaAziz <ayeshaaziz44436@gmail.com>
2025-07-15 01:42:45 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 //  https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/portfolio-portal/",
   server: {
   host: "::",
   port: 8080,
